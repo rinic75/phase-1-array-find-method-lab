66d91dcdbfad9c84e993765459be0039a444683f
chang <cshin7374@gmail.com>
2022-05-22 21:13:35 -0500

    commit tree 242e03ed7bbea78570bbc735e05ea34b851d5522

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr) {
+    let win = arr.find(item => item.result === 'W')
+    if (win === undefined) {
+        return undefined
+    } else {
+        return win.year;
+    }
+}
